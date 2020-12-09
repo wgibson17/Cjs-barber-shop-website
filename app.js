4a346d695bf7ede00ded0d72ff2c46e7bb555b1c
Jeff <sparklicorn@users.noreply.github.com>
2020-12-08 18:50:57 -0600

    Format for readability

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const hamburger = document.getElementById('hamburger'); // targets the hamburger
 const navUL = document.getElementById('nav-ul'); // targets the nav list
 
 hamburger.addEventListener('click', () => { // listens for clicking of hamburger
-    navUL.classList.toggle('show'); //adds show class to .nav-ul
+  navUL.classList.toggle('show'); //adds show class to .nav-ul
 });
